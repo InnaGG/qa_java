d99c9a687052c1a7bdd9c0036357e328e1ac6a08
InnaGG <innageldyeva@mail.ru>
2022-10-01 11:23:13 +0200

    added new test to cover getFood() method

--- src/test/java/com/example/CatTest.java
+++ src/test/java/com/example/CatTest.java
@@ -2,8 +2,13 @@ package com.example;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+import java.util.List;
 
+@RunWith(MockitoJUnitRunner.class)
 public class CatTest {
 
     @Mock
@@ -15,6 +20,15 @@ public class CatTest {
         String result = cat.getSound();
         String expected = "Мяу";
         Assert.assertEquals("Ожидаемый результат - \"Мяу!\"", expected, result);
+    }
+
+    @Test
+    public void getFoodTest() throws Exception {
+        Cat cat = new Cat(feline);
+        List<String> expectedResultList = List.of("Животные", "Птицы", "Рыба");
+        Mockito.when(feline.eatMeat()).thenReturn(List.of("Животные", "Птицы", "Рыба"));
+        List<String> actualResultList = cat.getFood();
+        Assert.assertEquals(expectedResultList, actualResultList);
     }
 
 }
